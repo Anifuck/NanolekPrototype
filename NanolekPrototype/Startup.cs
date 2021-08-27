f93128a1f072f22264cdbf0322b3fef3010894c5
Oleg <nsk-kok@yandex.ru>
2021-08-27 11:24:15 +0300

    Добавил функциональность регистрации пользователя

--- NanolekPrototype/Startup.cs
+++ NanolekPrototype/Startup.cs
@@ -8,6 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NanolekPrototype.Models;
 
 namespace NanolekPrototype
 {
@@ -23,6 +26,12 @@ namespace NanolekPrototype
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationContext>();
+
             services.AddControllersWithViews();
         }
 
@@ -44,6 +53,7 @@ namespace NanolekPrototype
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

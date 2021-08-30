f10621434429036d36d0b9dee6d91ca500745d37
Oleg <nsk-kok@yandex.ru>
2021-08-30 11:22:12 +0300

    Добавил инициализирование ролей и админа. Вынес настройки в appsettings

---
+++ NanolekPrototype/Initializers/RoleInitializer.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Protocols;
+using NanolekPrototype.Models;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace NanolekPrototype.Initializers
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(IConfiguration configuration, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            var adminEmail = configuration.GetSection("InitializationSettings").GetSection("AdminUserEmail").Value;
+            var adminPassword = configuration.GetSection("InitializationSettings").GetSection("AdminUserPassword").Value;
+
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await roleManager.FindByNameAsync("controller") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("controller"));
+            }
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User { Email = adminEmail, UserName = adminEmail};
+                IdentityResult result = await userManager.CreateAsync(admin, adminPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+    }
+}
--- NanolekPrototype/Program.cs
+++ NanolekPrototype/Program.cs
@@ -6,14 +6,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols;
+using NanolekPrototype.Initializers;
+using NanolekPrototype.Models;
 
 namespace NanolekPrototype
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var configuration = services.GetRequiredService<IConfiguration>();
+                    var userManager = services.GetRequiredService<UserManager<User>>();
+                    var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    await RoleInitializer.InitializeAsync(configuration, userManager, rolesManager);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>

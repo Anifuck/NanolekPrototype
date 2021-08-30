bd15799824639c39ee2b952b182eaf02c91dec98
Oleg <nsk-kok@yandex.ru>
2021-08-30 13:39:22 +0300

    Добавил таблицу протоколов упаковки

---
+++ NanolekPrototype/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NanolekPrototype.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<PackagingProtocol> PackagingProtocols { get; set; }
+    }
+}
---
+++ NanolekPrototype/Models/PackagingProtocol.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace NanolekPrototype.Models
+{
+    public class PackagingProtocol
+    {
+        public long Id { get; set; }
+        public Guid Guid { get; set; }
+        public string SerialNumber { get; set; }
+        public User ResponsibleUserOOK { get; set; }
+        public string StorageConditions { get; set; }
+        public double ShelfLife { get; set; }
+        public DateTime ManufacturingDate { get; set; }
+        public DateTime SellBy { get; set; }
+        public string PackageNumber { get; set; }
+        public User ResponsibleUserTLF { get; set; }
+        public string TradeName { get; set; }
+        public string SpecificationGP { get; set; }
+        public string InternalCodeGP { get; set; }
+        public Status Status { get; set; } = Status.InWork;
+        public string CancellationReason { get; set; }
+
+    }
+
+    public enum Status
+    {
+        InWork,
+        Completed,
+        Cancelled,
+    }
+
+}

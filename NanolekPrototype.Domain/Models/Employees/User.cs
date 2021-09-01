5000970d99fd68af54f0b5b71eb56cecdabb2ae6
Oleg <nsk-kok@yandex.ru>
2021-09-01 20:28:37 +0300

    Сделал аттрибутирование свойств модели пользователя

--- NanolekPrototype.Domain/Models/Employees/User.cs
+++ NanolekPrototype.Domain/Models/Employees/User.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace NanolekPrototype.EntityModels.Models
 {
     public class User : IdentityUser
     {
+        [DisplayName("Фио")]
         public string FullName { get; set; }
+        [DisplayName("Должность")]
         public string Position { get; set; }
     }
 }

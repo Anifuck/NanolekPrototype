aaf440f168d72df2bbc04c737d31e593046c02c9
Oleg <nsk-kok@yandex.ru>
2021-09-01 11:17:47 +0300

    Миграции

---
+++ NanolekPrototype.Context/Migrations/20210901081456_add allforms to PackagingProtocol.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NanolekPrototype.Context.Migrations
+{
+    public partial class addallformstoPackagingProtocol : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- NanolekPrototype.Domain/Models/PackagingProtocol.cs
+++ NanolekPrototype.Domain/Models/PackagingProtocol.cs
@@ -8,6 +8,7 @@ namespace NanolekPrototype.EntityModels.Models
     public class PackagingProtocol
     {
         public long Id { get; set; }
+
         public Guid Guid { get; set; }
         public string SerialNumber { get; set; }
         public User ResponsibleUserOOK { get; set; }
@@ -22,6 +23,16 @@ namespace NanolekPrototype.EntityModels.Models
         public string InternalCodeGP { get; set; }
         public PackagingProtocolStatus PackagingProtocolStatus { get; set; }
         public string CancellationReason { get; set; }
+
+
+        public ICollection<FormAssignmentForMarkingThermalTransferLabelOnCorrugatedBox> ForMarkingThermalTransferLabelOnCorrugatedBoxes { get; set; }
+        public ICollection<FormCheckingCheckweighingSetting> FormCheckingCheckweighingSettings { get; set; }
+        public ICollection<FormCheckingRejectionOfDefectiveTablet> FormCheckingRejectionOfDefectiveTablets { get; set; }
+        public ICollection<FormControlOfPrimaryPackaging> FormControlOfPrimaryPackagings { get; set; }
+        public ICollection<FormMaterialBalanceOfGPByLot> FormMaterialBalanceOfGpByLots { get; set; }
         public ICollection<FormReceptionAndMovementOfBulkProduct> FormReceptionAndMovementOfBulkProducts { get; set; }
+        public ICollection<FormReceptionAndMovementOfPackingMaterial> FormReceptionAndMovementOfPackingMaterials { get; set; }
+        public ICollection<FormSamplingFinishedProduct> FormSamplingFinishedProducts { get; set; }
+        public ICollection<FormSettingUpTechnologicalEquipment> FormSettingUpTechnologicalEquipments { get; set; }
     }
 }

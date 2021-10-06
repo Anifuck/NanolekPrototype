5e3ec8bfcb1c8b539523b942ad3194f2834b52df
Oleg <nsk-kok@yandex.ru>
2021-10-06 17:05:35 +0300

    Добавил перевод ролей на русский язык

---
+++ NanolekPrototype/Services/RoleTranslator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NanolekPrototype.Services
+{
+    public static class RoleTranslator
+    {
+        public static string GetRuRoleName(string roleName)
+        {
+            switch (roleName)
+            {
+                case "admin": return "Администратор";
+                case "executor": return "Исполнитель";
+                case "controller": return "Контролер";
+            }
+
+            return "";
+        }
+    }
+}

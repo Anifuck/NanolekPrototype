78550a0074c645cdcfdd4303a2547b25a1d69f76
Oleg <nsk-kok@yandex.ru>
2021-10-07 19:29:54 +0300

    Добавление модального окна проверки пароля  и написания причины для действий с формами

--- NanolekPrototype/wwwroot/js/AjaxController.js
+++ NanolekPrototype/wwwroot/js/AjaxController.js
@@ -1,4 +1,4 @@
-﻿    function PostApi(url, idButton, controllerName, element, id) {
+﻿function PostApi(url, controllerName, element, id) {
         $.post("/Form" + controllerName + "s/" + url + "/", { id: id })
             .done(function (data) {
                 console.log(data);
@@ -12,14 +12,73 @@
                     alert("ошибка");
             }
             );
+}
+
+
+function PostApi2(url, controllerName, elementId, id) {
+    var element = $("#" + elementId).parent();
+    $.post("/Form" + controllerName + "s/" + url + "/", { id: id })
+        .done(function (data) {
+                console.log(data);
+            if (data["status"] == 1) {
+                PopUpHide();
+                $("#" + controllerName).text(data["protocolState"]);
+                for (var i = 0; i <= element.children().length; i++)
+                    element.children()[0].remove();
+            }
+                else
+                    alert("ошибка");
+            }
+        );
+}
+
+
+function SendOnControl(formId, controllerName, elementId) {
+    PopUpShow("SendOnControlForm",formId, controllerName, elementId);
+}
+
+function SendOnApprove(formId, controllerName, elementId) {
+    PopUpShow("ApproveForm", formId, controllerName, elementId);
+}
+
+function SendOnRevision() {
+    alert("SendOnRevisionForm отработал");
+    /*PopUpShow("SendOnRevisionForm", formId, controllerName, elementId);*/
+}
+
+function CheckPassword() {
+    if ($("#passwordText").val() == "1") {
+        var formId = $("#formId").val();
+        var controllerName = $("#controllerName").val();
+        var elementId = $("#elementId").val();
+        var url = $("#url").val();
+        PostApi2(url, controllerName, elementId, formId);
+        $("#layoutBackground").css("pointer-events", "auto");
+    } else {
+        alert("Пароль неверный, попробуйте еще раз")
     }
+}
 
+//$("#popup").hide();
 
-    function SendOnControl(id, controllerName,element) {
-        PostApi("SendOnControlForm", "onControlButton", controllerName, element, id);
+//Функция отображения PopUp
+function PopUpShow(url, formId, controllerName, elementId) {
+    $("#popup").show();
+    $("#formId").val(formId);
+    $("#controllerName").val(controllerName);
+    $("#elementId").val(elementId);
+    $("#url").val(url);
+    $("#layoutBackground").css("pointer-events", "none");
+}
+
+//Функция скрытия PopUp
+    function PopUpHide() {
+        $("#popup").hide();
+        $("#formId").val("");
+        $("#controllerName").val("");
+        $("#elementId").val("");
+        $("#url").val("");
+        $("#passwordText").val("");
+        $("#cause").val("");
+        $("#layoutBackground").css("pointer-events", "auto");
     }
-
-    function SendOnApprove(id, controllerName, element) {
-        PostApi("ApproveForm", "ApproveAndOnRevision", controllerName, element, id);
-    }
-
